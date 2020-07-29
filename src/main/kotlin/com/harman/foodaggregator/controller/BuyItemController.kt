376850c237959599463489bd024b48ac76be51e5
Hirenshah001 <55998629+Hirenshah001@users.noreply.github.com>
2020-07-30 00:00:05 +0530

    Add comment in RestController

--- src/main/kotlin/com/harman/foodaggregator/controller/BuyItemController.kt
+++ src/main/kotlin/com/harman/foodaggregator/controller/BuyItemController.kt
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
+/**
+ * This Rest Controller accepts request of buy-item
+ *
+ */
+
 @RestController
 class BuyItemController {
 
